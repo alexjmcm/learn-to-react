8a199bf0601b2e942fc91a7660ba1f2b1a166fe4
Sonam Mankame <sonamrohit.ms25@gmail.com>
2018-11-20 20:55:12 -0500

    added result page

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+      Learn to React
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/lesson"
+              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+            >
+            Lesson
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Result"
+              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+            >
+              Result
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/resultpage/resultpage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../components/Container";
+
+
+
+function Result() {
+  return (
+    <div>
+      
+      <Container style={{ marginTop: 30 }}>
+      
+        <p> Congratulations you have completed the project successfully. 
+            Please refer to the suggested todo projects below</p>
+            <div>  <Link
+              to="/lesson"
+              className={
+                window.location.pathname === "/lesson"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Go back to Lesson Page
+            </Link>    </div>
+       
+      </Container>
+       </div>
+    
+  );
+}
+
+export default Result;
