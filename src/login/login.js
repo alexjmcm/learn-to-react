0cbccff29e1272c62e3bb82216907477792e82e9
Sonam Mankame <sonamrohit.ms25@gmail.com>
2018-11-27 19:13:38 -0500

    added style to login page

--- src/login/login.js
+++ src/login/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
-
+import "./style.css"
 
 
 function Login() {
@@ -10,23 +10,23 @@ function Login() {
       
       <Container style={{ marginTop: 30 }}>
       
-      <div class="field">
-        <label class="label">Username</label>
-        <div class="control">
-          <input class="input" type="username" placeholder="e.g Alex Smith">
+      <div className="field">
+        <label className="label">Username</label>
+        <div className="control">
+          <input className="input" type="username" placeholder="e.g Alex Smith">
           </input>
         </div>
         </div>
       
-        <div class="field">
-        <label class="label">Password</label>
-        <div class="control">
-          <input class="input" type="password" ></input>
+        <div className="field">
+        <label className="label">Password</label>
+        <div className="control">
+          <input className="input" type="password" ></input>
         </div>
         </div>
 
         <div class="control">
-        <button class="button is-info">Submit</button>
+        <button class=" button submit is-info">Submit</button>
         </div>
 
        
