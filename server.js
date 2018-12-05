9e1dac37078b81b07f061fc46384f0e546b0a294
Alex Monteiro <alexjmcm@Alexs-MacBook-Pro.local>
2018-12-04 20:49:35 -0500

    server.js

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
+
+
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
